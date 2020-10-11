ffa148dd488cb5d5ac4a4bca242b79da1da17b8e
Devendra Johari <54894480+DevendraJohari24@users.noreply.github.com>
2020-10-11 14:34:10 +0530

    hi

---
+++ pics/lib/src/app.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'widgets/image_list.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              fetchImage();
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some Images'),
+        ),
+        body: ImageList(images),
+      ),
+    );
+  }
+}
